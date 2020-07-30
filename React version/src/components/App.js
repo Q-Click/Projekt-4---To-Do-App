3c7328c07a7eecbef87b195c1c23a9ef0852df0a
Adam Kukliński <64474051+Q-Click@users.noreply.github.com>
2020-07-30 13:17:18 +0200

    Initial commit

---
+++ React version/src/components/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+import "./App.css";
+
+class App extends Component {
+  counter = 9;
+  state = {
+    tasks: [],
+  };
+
+  deleteTask = (id) => {
+    let tasks = [...this.state.tasks];
+    tasks = tasks.filter((task) => task.id !== id);
+    this.setState({
+      tasks,
+    });
+  };
+
+  changeTaskStatus = (id) => {
+    const tasks = Array.from(this.state.tasks);
+    tasks.forEach((task) => {
+      if (task.id === id) {
+        task.active = false;
+        task.finishDate = new Date().getTime();
+      }
+    });
+    this.setState({
+      tasks,
+    });
+  };
+
+  addTask = (text, date, important) => {
+    const task = {
+      id: this.counter,
+      text,
+      date,
+      important,
+      active: true,
+      finishDate: null,
+    };
+    this.counter++;
+    console.log(task, this.counter);
+
+    this.setState((prevState) => ({
+      tasks: [...prevState.tasks, task],
+    }));
+
+    return true;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TO DO APP</h1>
+        <AddTask add={this.addTask} />
+        <TaskList
+          tasks={this.state.tasks}
+          delete={this.deleteTask}
+          change={this.changeTaskStatus}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
