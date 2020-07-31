bab340c10d781c0bccee8c8d2453ec33f0ed3b9e
Adam Kukliński <64474051+Q-Click@users.noreply.github.com>
2020-07-31 14:07:14 +0200

    Initial Commit

---
+++ build/precache-manifest.66f9e449c6ef659e92e6c70b8448a935.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db0411e3028421cb5572",
+    "url": "/Projekt-4---To-Do-App/static/css/main.ba0cba8f.chunk.css"
+  },
+  {
+    "revision": "db0411e3028421cb5572",
+    "url": "/Projekt-4---To-Do-App/static/js/main.db0411e3.chunk.js"
+  },
+  {
+    "revision": "eefc8de293d1d7f2c7a1",
+    "url": "/Projekt-4---To-Do-App/static/js/1.eefc8de2.chunk.js"
+  },
+  {
+    "revision": "db921f0d7b7b6f90b11a",
+    "url": "/Projekt-4---To-Do-App/static/js/runtime~main.db921f0d.js"
+  },
+  {
+    "revision": "1a4618e73bd31c7077a83fd646f1b945",
+    "url": "/Projekt-4---To-Do-App/index.html"
+  }
+];
